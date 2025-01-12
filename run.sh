f008d68037804283e68661c998ac4c5ed4896188
Zikun Zhu <zhu.zikun@u.nus.edu>
2025-01-12 14:36:36 +0800

    fix: improve GitHub Actions deployment
    - Add Java 11 setup
    
    - Add force_orphan for clean gh-pages
    
    - Add timezone and overwrite flags
    
    - Improve debugging output

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Downloads a specific version of RepoSense.jar of your choice from our repository
-./get-reposense.py --tag v2.8
+./get-reposense.py --tag v2.8 --overwrite
 
 # Executes RepoSense
 java --add-opens java.base/java.time=ALL-UNNAMED -jar RepoSense.jar \
     --config ./configs \
     --since 01/01/2020 \
     --until 31/12/2024 \
-    --formats "java md txt"
+    --formats "java md txt" \
+    --timezone UTC+08
