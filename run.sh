12f8b4c73af9a2690cb9e44fc2a10bd3b85f455e
Zikun Zhu <zhu.zikun@u.nus.edu>
2025-01-12 14:31:14 +0800

    feat: configure RepoSense to track 3 iP repositories

--- run.sh
+++ run.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
 # Downloads a specific version of RepoSense.jar of your choice from our repository
-## Examples of supported options:
-### ./get-reposense.py --release               # Gets the latest release (Stable)
-### ./get-reposense.py --master                # Gets the latest master (Beta)
-### ./get-reposense.py --tag v1.6.1            # Gets a specific version
-### ./get-reposense.py --latest v1.6           # Gets the latest version with the given tag prefix e.g. v1.6.1
-### ./get-reposense.py --commit abc123         # Gets a specific commit
-### ./get-reposense.py --release --overwrite   # Overwrite RepoSense.jar, if exists, with the latest release
-
-./get-reposense.py --release
+./get-reposense.py --tag v2.8
 
 # Executes RepoSense
-# Do not change the default output folder name (reposense-report)
-## Examples of other valid options; For more, please view the user guide
-### java -jar RepoSense.jar --repos https://github.com/reposense/RepoSense.git
-
-java -jar RepoSense.jar --config ./configs
+java --add-opens java.base/java.time=ALL-UNNAMED -jar RepoSense.jar \
+    --config ./configs \
+    --since 01/01/2020 \
+    --until 31/12/2024 \
+    --formats java;md;txt
